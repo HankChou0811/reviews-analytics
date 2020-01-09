d6b6f59e40f7a1acd738a59f7738e0c189eb8460
Hank Chou <kkkabc7419631@hotmail.com>
2020-01-09 17:49:00 +0800

    second version

--- read.py
+++ read.py
@@ -8,4 +8,9 @@ with open('reviews.txt','r') as f:
 		if count % 1000 == 0: # %是求餘數 也就是"如果count跟一千的餘數是0:"
 			print(len(data))
 
-print(data[10])
+print('檔案讀取完畢，共有', len(data), '資料')
+
+sum_len = 0
+for d in data: #每個D都是個字串
+	sum_len = sum_len + len(d)
+print('the average len of reviews is', sum_len/len(data))
