4893768abcc527f5104520f1c4f8a065301a89e2
Hank Chou <kkkabc7419631@hotmail.com>
2020-02-03 19:12:06 -0800

    loading upgrade

--- read.py
+++ read.py
@@ -1,12 +1,18 @@
+import time
+import progressbar
+
+
 data = []
 count = 0 #透過記數來了解程式運算狀況的一種方法
+bar = progressbar.ProgressBar(max_value=1000000)
 with open('reviews.txt','r') as f:
 	for line in f:
 		data.append(line)
 #以上為一套把檔案載入的公式
 		count += 1 # 也就是count = count + 1
-		if count % 1000 == 0: # %是求餘數 也就是"如果count跟一千的餘數是0:"
-			print(len(data))
+		bar.update(count)
+		# if count % 1000 == 0: # %是求餘數 也就是"如果count跟一千的餘數是0:"
+		# 	print(len(data))
 
 print('檔案讀取完畢，共有', len(data), '資料')
 
